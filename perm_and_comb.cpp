c83c9c78c607cb7b7737610ca68631eed02778ca
aniket <aniketkrsharma2309@gmail.com>
2020-01-24 15:40:49 +0530

    2nd commit

--- perm_and_comb.cpp
+++ perm_and_comb.cpp
@@ -1,17 +1,47 @@
 #include<iostream>
+
 using namespace std;
 
+
+
 int permutation(int n, int r)
+
 {
-  return 0;
+	if(r==0)
+	return 1;
+	else 
+	return( n*permutation(n-1,r-1));
 }
+
+
+
 
 int combination(int n, int r)
+
 {
-  return 0;
+	if(r==0)
+	return 1;
+	else
+	return((n*permutation(n-1,r-1))/r);
+	
+
 }
 
+
 int main()
+
 {
+	int n,r,nPr,nCr;
+	cout<<"Enter n :";
+	cin>>n;
+	cout<<"Enter r :";
+	cin>>r;
+	nPr =permutation(n,r);
+	nCr =combination(n,r);
+	cout<<"nPr is :"<<nPr<<endl;
+	cout<<"nCr is :"<<nCr<<endl;
+	
+
   return 0;
+
 }
